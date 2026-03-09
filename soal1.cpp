6aa5d5fcccb5096147eaa944e43e71484d415fc9
addinsyahidn <addinsyahidn@gmail.com>
2026-03-09 21:05:54 +0700

    Update soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -1,5 +1,5 @@
 // Soal ini dijawab menggunakan bahasa C, tapi tidak apa-apa jika menggunakan extension CPP
-
+//tes
 /* 
 Buat program yang meminta pengguna memasukkan sebuah kata (terdiri dari minimal 10 karakter), 
 lalu tampilkan karakter per karakter menggunakan putchar().
