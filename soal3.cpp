b05249823b76e5c414a08f7fe488ea9baed0e1a0
addinsyahidn <addinsyahidn@gmail.com>
2026-03-09 11:10:57 +0700

    Update soal3.cpp

--- soal3.cpp
+++ soal3.cpp
@@ -4,3 +4,11 @@
     Buatlah program yang menerima input satu bilangan bulat (detik) 
     dan menampilkan output dalam format: X detik = H jam M menit S detik
 */
+
+#include <stdio.h>
+
+int main() {
+    int dtk;
+    printf("%d detik = %d jam %d menit %d detik", dtk, (1), (1), (1), (1)); //untuk sementara
+    return 0;
+}
